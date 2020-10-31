6d03666466234372c1ec7a4144fada8f60e9fa50
ykg8301 <ana31005@gmail.com>
2020-10-31 19:20:13 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
+// import axios from 'axios'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    account: {
+      email: '',
+      password: ''
+    }
   },
   mutations: {
+
   },
   actions: {
+    // login () {
+    //   const url = 'https://hexschool-tutorial.herokuapp.com/api/signin'
+    //   axios.post(url).then((res) => {
+    //     if (res.data.success === false) {
+    //       alert(res.data.message)
+    //     }
+    //   })
+    // },
+    // registered () {
+    //   const api = 'https://hexschool-tutorial.herokuapp.com/api/signup'
+    //   axios.post(api).then((data) => {
+    //     console.log('123', data)
+    //   })
+    // }
   },
   modules: {
   }
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <form class="form-box">
+        <div class="form-group">
+          <label>信箱</label>
+          <input type="email" class="form-control"  placeholder="Enter email" v-model="account.email" name="account">
+        </div>
+        <div class="form-group">
+          <label>密碼</label>
+          <input type="password" class="form-control" placeholder="Password" v-model="account.password" name="password">
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+      <button type="submit" class="btn btn-primary" @click.prevent="login()">登入</button>
+      <button type="submit" class="btn btn-danger ml-3" @click.prevent="registered()">註冊</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      account: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      const xhr = new XMLHttpRequest()
+      xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signin', true)
+      xhr.setRequestHeader('Content-Type', 'application/json')
+      const data = JSON.stringify(this.account)
+      xhr.send(data)
+      xhr.onload = function () {
+        console.log(xhr.responseText)
+      }
+      // const url = 'https://hexschool-tutorial.herokuapp.com/api/signin'
+      // this.$http.post(url).then((res) => {
+      //   if (res.data.success === false) {
+      //     alert(res.data.message)
+      //   }
+      // })
+    },
+    registered () {
+      const xhr = new XMLHttpRequest()
+      xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signup', true)
+      xhr.setRequestHeader('Content-Type', 'application/json')
+      const data = JSON.stringify(this.account)
+      xhr.send(data)
+      xhr.onload = function () {
+        console.log(xhr.responseText)
+      }
+      // const api = 'https://hexschool-tutorial.herokuapp.com/api/signup'
+      // this.$http.post(api).then((data) => {
+      //   console.log('123', data)
+      // })
+    }
+  }
+}
+</script>
